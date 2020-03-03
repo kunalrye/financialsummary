edf33dd07908dfe3258fee38c2fcef8cea499bc8
kunalrye <ksrai99@gmail.com>
2020-03-02 18:01:39 -0600

    functionalized separate_sections

--- wrangling/separate_sections.py
+++ wrangling/separate_sections.py
@@ -2,44 +2,52 @@
 Take each text document, and create a dictionary of the document structure by identifying
 headers and their respective content
 """
+import os
 import re
 from collections import defaultdict
 
-## NOT DONE YET!
 
-def structure_txt(files):
+def separate_document(directory):
     """
-    :param files: list of files that need to be structured
-    :return: nested dict with filename as key; then key,value pairing is subheading and content
+    :param directory: directory containing the text files
+    :return:nested dict
     """
-    for file in files:
 
-        f = ("/Users/kunal/Dropbox/batch_0005/001/AAPL_0000320193_10Q_20171230_Item2_excerpt.txt")
+    full_dict = {}
+    for subdir, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.txt'):
+                name_path = os.path.join(subdir, filename)
+                filename_list = filename.rsplit('_')
+                # print(filename_list)
+                file_key = filename_list[0] + "_" + filename_list[1] + "_" + filename_list[3]
+                with open(name_path) as f:
+                    my_list = f.readlines()
+                    list2 = list(filter(('\n').__ne__, my_list))
+                    my_list = list2
+                    headers = []
+                    content = []
+                    doc_structure = {}
+                    for x in my_list:
+                        # throw out all "\n" characters from the list, and find all content sections
+                        if 15 <= len(x) <= 100:
+                            headers.append(x)
+                        elif len(x) > 100:
+                            content.append(x)
+                        else:
+                            continue
+                    # Determine the position of the content relative to the header and map to dictionary
+                    for x in my_list:
+                        for y in headers:
+                            if y in my_list and (my_list.index(y) != my_list.index(my_list[-1])):
+                                header_idx = my_list.index(y)
+                                content_idx = header_idx + 1
+                                doc_structure[y] = my_list[content_idx]
+                            elif y in my_list and (my_list.index(y) == my_list[-1]):
+                                pass
+                    full_dict[file_key] = doc_structure
+                    f.close()
+    return full_dict
 
-        with open("/Users/kunal/Dropbox/batch_0005/001/AAPL_0000320193_10Q_20171230_Item2_excerpt.txt") as f:
-            my_list = f.readlines()
-            # remove all '\n' characters from the list
-            list2 = list(filter(('\n').__ne__, my_list))
-            my_list = list2
-            headers = []
-            content = []
-            doc_structure = {}
-            # figure out what is a header and what is not
-            for x in my_list:
-                # print(x)
-                if 15 <= len(x) <= 100:
-                    headers.append(x)
-                elif len(x) > 100:
-                    content.append(x)
-                else:
-                    continue
-            # iterate through list and create dictionary of the file
-            for x in my_list:
-                for y in headers:
-                    if y in my_list and (my_list.index(y) != my_list.index(my_list[-1])):
-                        header_idx = my_list.index(y)
-                        content_idx = header_idx + 1
-                        doc_structure[y] = my_list[content_idx]
-                    elif y in my_list and (my_list.index(y) == my_list[-1]):
-                        break
-            print(doc_structure)
+
+# separate_document("/Users/kunal/Dropbox/batch_0005/)
