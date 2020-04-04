609904fcb1bfd64b4f96304444a5df29d4306064
Angel <aas14@rice.edu>
2020-04-04 16:51:39 -0500

    Lexrank Luhn and LSA models

--- modeling/LexRank_Luhn_LSA.py
+++ modeling/LexRank_Luhn_LSA.py
@@ -1,28 +1,39 @@
 import sumy
+import summa
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
 from sumy.summarizers.lsa import LsaSummarizer
+from modeling.run_model import summarize_test_docs, summarize_train_docs
 
-def LexRankSummary(file, sentences):
-    parser = PlaintextParser.from_file(file, Tokenizer("english"))
+
+def LexRankSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
     summarizer = LexRankSummarizer()
     summary = summarizer(parser.document, sentences)
     for sentence in summary:
         print(sentence)
 
-def LuhnSummary(file, sentences):
-    parser = PlaintextParser.from_file(file, Tokenizer("english"))
+def LuhnSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
     summarizer = LuhnSummarizer()
     summary = summarizer(parser.document, sentences)
     for sentence in summary:
         print(sentence)
 
-def LsaSummary(file, sentences):
-    parser = PlaintextParser.from_file(file, Tokenizer("english"))
+def LsaSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
     summarizer = LsaSummarizer()
     summary = summarizer(parser.document, sentences)
     for sentence in summary:
         print(sentence)
 
+#summarize_train_docs(LexRankSummary, "lexrank")
+#summarize_test_docs(LexRankSummary, "lexrank")
+
+#summarize_train_docs(LuhnSummary, "luhn")
+#summarize_test_docs(LuhnSummary, "luhn")
+
+#summarize_train_docs(LsaSummary, "lsa")
+#summarize_test_docs(LsaSummary, "lsa")
