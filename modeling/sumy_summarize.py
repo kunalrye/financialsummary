9a73a2be71b6a0dc5e005f5c6cce3d88c4668e47
joshuabae <jsb9@rice.edu>
2020-04-06 19:44:46 -0500

    text parsing and kmeans modeling

---
+++ modeling/sumy_summarize.py
@@ -0,0 +1,22 @@
+from modeling.run_model import summarize_test_docs, summarize_train_docs
+from modeling.LexRank_Luhn_LSA import LexRankSummary, LuhnSummary, LsaSummary
+import os
+
+os.chdir('C:/Users/jsbae/financialsummary/resources/legal_filter_train/ADI')
+print(os.getcwd())
+
+# summarize_train_docs('ADI', LexRankSummary(open('ADI_0000006281_20180505_item1.txt')), 'LexRank')
+
+adi_item1 = open('ADI_0000006281_20180505_item1.txt', encoding='utf-8').read().splitlines()
+adi_item2 = open('ADI_0000006281_20180505_item2.txt', encoding='utf-8').read().splitlines()
+adi_item3 = open('ADI_0000006281_20180505_item3.txt', encoding='utf-8').read().splitlines()
+adi_item4 = open('ADI_0000006281_20180505_item4.txt', encoding='utf-8').read().splitlines()
+adi_part2 = open('ADI_0000006281_20180505_part2.txt', encoding='utf-8').read().splitlines()
+entire_corpus = adi_item1 + adi_item2 + adi_item3 + adi_item4
+
+LexRankSummary(entire_corpus, 10)
+print('\n')
+LuhnSummary(entire_corpus, 10)
+print('\n')
+LsaSummary(entire_corpus, 10)
+
