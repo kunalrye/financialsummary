cb626c5510fdf7d1085f431a8399f00e77d464fc
joshuabae <jsb9@rice.edu>
2020-04-08 15:54:45 -0500

    added new summarizer functions

--- modeling/LexRank_Luhn_LSA.py
+++ modeling/LexRank_Luhn_LSA.py
@@ -6,6 +6,10 @@ from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
 from sumy.summarizers.lsa import LsaSummarizer
 from sumy.summarizers.edmundson import EdmundsonSummarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.summarizers.sum_basic import SumBasicSummarizer
+from sumy.summarizers.kl import KLSummarizer
+from sumy.summarizers.reduction import ReductionSummarizer
 
 from modeling.run_model import summarize_test_docs, summarize_train_docs
 
@@ -34,9 +38,41 @@ def LsaSummary(document, sentences):
     #     print(sentence)
     return summary
 
-def EdmundsonSummarizer(document, sentences):
+def EdmundsonSummary(document, sentences):
     parser = PlaintextParser.from_string(document,Tokenizer("english"))
     summarizer = EdmundsonSummarizer()
+    summary = summarizer(parser.document, sentences)
+    # for sentence in summary:
+    #     print(sentence)
+    return summary
+
+def TextRankSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
+    summarizer = TextRankSummarizer()
+    summary = summarizer(parser.document, sentences)
+    # for sentence in summary:
+    #     print(sentence)
+    return summary
+
+def SumBasicSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
+    summarizer = SumBasicSummarizer()
+    summary = summarizer(parser.document, sentences)
+    # for sentence in summary:
+    #     print(sentence)
+    return summary
+
+def KLSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
+    summarizer = KLSummarizer()
+    summary = summarizer(parser.document, sentences)
+    # for sentence in summary:
+    #     print(sentence)
+    return summary
+
+def ReductionSummary(document, sentences):
+    parser = PlaintextParser.from_string(document,Tokenizer("english"))
+    summarizer = ReductionSummarizer()
     summary = summarizer(parser.document, sentences)
     # for sentence in summary:
     #     print(sentence)
